3a02b20e4bf28a864818014558233ea86850b3bb
lexussab <lexussab@gmail.com>
2014-04-30 12:33:15 +0400

    Update Program

--- Experiments/Program.java
+++ Experiments/Program.java
@@ -5,15 +5,15 @@ import java.util.zip.GZIPOutputStream;
 
 public class Program {
 	
-	//Задаем имя и путь к начальному файлу
-	public static final String fName1 = ".."+ File.separator +"test1.txt";
+	//Р—Р°РґР°РµРј РёРјСЏ Рё РїСѓС‚СЊ Рє РЅР°С‡Р°Р»СЊРЅРѕРјСѓ С„Р°Р№Р»Сѓ
+	public static final String fName1 = ".."+ File.separator +"test3.txt";
 	
-	//Задаем имя и путь к конечному файлу
+	//Р—Р°РґР°РµРј РёРјСЏ Рё РїСѓС‚СЊ Рє РєРѕРЅРµС‡РЅРѕРјСѓ С„Р°Р№Р»Сѓ
 	public static final String fName2 = ".."+ File.separator +"test2.txt";
 	public static final String fNameGZIP = ".."+ File.separator +"test2.gzip";
 
 	public static void main(String[] args) throws IOException {
-		
+
 		File f1 = new File(fName1);
 		if (f1.exists()) {
 			
@@ -21,36 +21,44 @@ public class Program {
 			FileWriter fw = new FileWriter(fName2);
 			String s;
 			
-			//Проверяем размер файла
+			//РџСЂРѕРІРµСЂСЏРµРј СЂР°Р·РјРµСЂ С„Р°Р№Р»Р°
 			if (f1.length() <= 10485760){
-				
-				//Чтение данных и перекодирование в новый файл
-				while ((s = reader.readLine()) != null){
-					byte[] b_s = s.getBytes("cp1251");
-					String str = new String(b_s, "UTF8");
-					fw.write(str + "\r\n");
+				try{
+					//Р§С‚РµРЅРёРµ РґР°РЅРЅС‹С… Рё РїРµСЂРµРєРѕРґРёСЂРѕРІР°РЅРёРµ РІ РЅРѕРІС‹Р№ С„Р°Р№Р»
+					while ((s = reader.readLine()) != null){
+						byte[] b_s = s.getBytes("cp1251");
+						String str = new String(b_s, "UTF8");
+						fw.write(str + "\r\n");
+					}
+				} finally{
+					fw.flush();
+					reader.close();
+					fw.close();
 				}
-				reader.close();
-				fw.close();
 			}else {
 				
-		        GZIPOutputStream out = new GZIPOutputStream(new FileOutputStream(fNameGZIP));
+			BufferedOutputStream out = new BufferedOutputStream(
+					      new GZIPOutputStream(
+					        new FileOutputStream(fNameGZIP)), 1024);
+//		        GZIPOutputStream out = new GZIPOutputStream(new FileOutputStream(fNameGZIP));
 		        
-				//Чтение данных и перекодирование в новый файл
-				while ((s = reader.readLine()) != null){
-					byte[] b_s = s.getBytes("cp1251");
-					String str = new String(b_s, "UTF8");
-					byte[] buf = str.getBytes("UTF8");
-					out.write(buf);	
+				try{
+			        //Р§С‚РµРЅРёРµ РґР°РЅРЅС‹С… Рё РїРµСЂРµРєРѕРґРёСЂРѕРІР°РЅРёРµ РІ РЅРѕРІС‹Р№ С„Р°Р№Р»
+					while ((s = reader.readLine()) != null){
+						byte[] b_s = s.getBytes("cp1251");
+						String str = new String(b_s, "UTF8");
+						byte[] buf = str.getBytes("UTF8");
+						out.write(buf);	
+					}
+				} finally{
+					out.flush();
+			        out.close();
+					reader.close();
+					fw.close();
 				}
-				out.finish();
-		        out.close();
-				reader.close();
-				fw.close();
 			}
 
 		}else
 			System.out.println("File not found");
-	
 	}
 }
